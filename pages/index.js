2572e17a2e99b59880b3b4899f9cc33264eeb65c
christopher-koch <info@ckah.de>
2023-03-07 12:15:17 +0100

    add simple success Message if Form input is correct

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { validUrlCharacters } from "@/data/valid-url-characters";
 import Link from "next/link";
+import { useState } from "react";
 
 function generateID() {
   let randomText = "";
@@ -11,29 +12,34 @@ function generateID() {
   }
   return randomText;
 }
-//Get value from input
-const formData = new FormData(event.target);
-const data = Object.fromEntries(formData);
-const { input } = data;
 
 export default function Home({ shortUrls, setShortUrls }) {
+  const [successForm, setSuccessForm] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Extend later with ID and other stuff -  or at another point?
-    shortUrls.map((element) => {
-      const shortURL = generateID();
-      setShortUrls([
-        ...shortUrls,
-        { ...element, longURL: input, shortURL: shortURL, id: shortURL },
-      ]);
-    });
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const { input } = data;
+    const newUrl = new URL(input);
+    const shortURL = generateID();
+    // Extend later with ID and other stuff - or at another point?
+    setShortUrls([
+      ...shortUrls,
+      { longURL: input, shortURL: shortURL, id: shortURL },
+    ]);
+    if (newUrl.protocol === "http:" || newUrl.protocol === "https:") {
+      console.log("Yay!");
+      setSuccessForm(!false);
+      console.log(successForm);
+    }
     event.target.reset();
   };
 
   return (
     <main>
       <Heading>Paste a long URL and click the Shorten-Button</Heading>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} aria-label="URL Shortener Form">
         <input
           name="input"
           id="input"
@@ -43,6 +49,7 @@ export default function Home({ shortUrls, setShortUrls }) {
         />
         <button>Shorten URL</button>
       </form>
+      {successForm === true ? <p>Du bist der geilste!</p> : null}
       <p>
         Here will stand some additional info about this service. For example, a
         link to the Dashboard to see all links.
