49fd70573a6ba7e11a90d6d35d357ca0f12ec76d
christopher-koch <info@ckah.de>
2023-03-07 15:02:48 +0100

    add redirect if user goes to short-url that is not available

--- pages/[id].js
+++ pages/[id].js
@@ -1,17 +1,29 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function RedirectComponent({ shortUrls }) {
   const router = useRouter();
   const { id } = router.query;
+  let isShortUrl = true;
 
   shortUrls.find((url) => {
-    id === url.id ? router.push(url.longURL) : null;
+    id === url.id ? router.push(url.longURL) : (isShortUrl = false);
   });
 
   return (
     <>
-      <h2>Please hold the line</h2>
-      <p>You are on being redirected to {shortUrls.longURL}</p>
+      {isShortUrl === true ? (
+        <>
+          <h2>Please hold the line.</h2>
+          <p>You are being redirected to {shortUrls.longURL}</p>
+        </>
+      ) : (
+        <p>
+          This short URL is not available.{" "}
+          <Link href="/">Click here to go back</Link>
+        </p>
+      )}
     </>
   );
 }
