a27f9552238037bf9d33a93abd4f77ff9e17fc63
christopher-koch <info@ckah.de>
2023-03-22 14:43:58 +0100

    add default pages in ...nextautt file

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,12 +11,20 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-  callbacks: {
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
+  // always return Internal Server Error (500)
+  /* callbacks: {
     async redirect() {
       signIn(undefined, {
         callbackUrl: process.env.NEXT_PUBLIC_VERCEL_URL,
       });
     },
-  },
+  }, */
 };
 export default NextAuth(authOptions);
