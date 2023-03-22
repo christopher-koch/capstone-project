9084c4c9ed68bdc48367e247c0c78f9ff63066b3
christopher-koch <info@ckah.de>
2023-03-22 12:32:25 +0100

    set callback url after login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { redirect } from "next/dist/server/api-utils";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -10,5 +11,10 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async redirect(url, baseUrl) {
+      return `${NEXT_PUBLIC_VERCEL_URL}`;
+    },
+  },
 };
 export default NextAuth(authOptions);
