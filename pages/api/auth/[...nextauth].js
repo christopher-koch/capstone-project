58b28cd24547d4e96fdf7d7024bea808b447c2e4
christopher-koch <info@ckah.de>
2023-03-22 14:46:57 +0100

    only redirect to / on first sign in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,11 +12,11 @@ export const authOptions = {
     // ...add more providers here
   ],
   pages: {
-    signIn: "/auth/signin",
+    /* signIn: "/auth/signin",
     signOut: "/auth/signout",
     error: "/auth/error", // Error code passed in query string as ?error=
-    verifyRequest: "/auth/verify-request", // (used for check email message)
-    newUser: "/auth/new-user", // New users will be directed here on first sign in (leave the property out if not of interest)
+    verifyRequest: "/auth/verify-request", */ // (used for check email message)
+    newUser: "/", // New users will be directed here on first sign in (leave the property out if not of interest)
   },
   // always return Internal Server Error (500)
   /* callbacks: {
